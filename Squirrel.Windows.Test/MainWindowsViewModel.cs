91006de2acd74ffd4a8c0431bb64a36ba5cd9455
kuttsun <tsukushimail+github@gmail.com>
2018-02-05 00:56:43 +0900

    viewmodel修正

--- Squirrel.Windows.Test/MainWindowsViewModel.cs
+++ Squirrel.Windows.Test/MainWindowsViewModel.cs
@@ -7,12 +7,18 @@ using System.Threading.Tasks;
 using System.Reflection;
 using System.Windows;
 
+using Prism.Mvvm;
+using Prism.Commands;
+
+using Squirrel;
+
 namespace Squirrel.Windows.Test
 {
-    class MainWindowsViewModel
+    class MainWindowsViewModel : BindableBase
     {
         public string Path { get; set; }
         public string Version { get; set; }
+        public DelegateCommand Button { get; set; }
 
         public MainWindowsViewModel()
         {
@@ -21,6 +27,14 @@ namespace Squirrel.Windows.Test
             Path = assembly.Location;
             //バージョン番号を表示
             Version = assembly.GetName().Version.ToString();
+
+            Button = new DelegateCommand(async () =>
+            {
+                using (var mgr = new UpdateManager("C:\\Projects\\MyApp\\Releases"))
+                {
+                    var ret = await mgr.CheckForUpdate();
+                }
+            });
         }
     }
 }
