608b1570bca84101287b58fd652a8d53f8f87bdd
kuttsun <tsukushimail+github@gmail.com>
2018-02-05 09:49:48 +0900

    Add ConsoleApp

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Reflection;
+
+using Squirrel;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            // Exeの場所を表示
+            Console.WriteLine($"Path: {assembly.Location}");
+            // バージョン番号を表示
+            Console.WriteLine($"Version: {assembly.GetName().Version.ToString()}");
+
+            //using (var mgr = new Updateanager(@"C:\projects\myapp\releases"))
+            //{
+            //    var ret = await mgr.checkforupdate();
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
