ac3185d48165bbeb91cffc5ddd7672daad49ad69
kuttsun <tsukushimail+github@gmail.com>
2018-02-06 19:16:09 +0900

    GUI修正

--- Squirrel.Windows.Test/MainWindowsViewModel.cs
+++ Squirrel.Windows.Test/MainWindowsViewModel.cs
@@ -16,9 +16,27 @@ namespace Squirrel.Windows.Test
 {
     class MainWindowsViewModel : BindableBase
     {
-        public string Path { get; set; }
-        public string Version { get; set; }
-        public DelegateCommand Button { get; set; }
+        public string Path
+        {
+            get { return path; }
+            set { SetProperty(ref path, value); }
+        }
+        string path;
+
+        public string Version
+        {
+            get { return version; }
+            set { SetProperty(ref version, value); }
+        }
+        string version;
+        public string Result
+        {
+            get { return result; }
+            set { SetProperty(ref result, value); }
+        }
+        string result;
+        public DelegateCommand UpdateCheckLocalButton { get; set; }
+        public DelegateCommand UpdateCheckGitHubButton { get; set; }
 
         public MainWindowsViewModel()
         {
@@ -28,33 +46,67 @@ namespace Squirrel.Windows.Test
             //バージョン番号を表示
             Version = assembly.GetName().Version.ToString();
 
-            Button = new DelegateCommand(async () =>
+            UpdateCheckLocalButton = new DelegateCommand(async () =>
             {
                 await Task.Run(() => CheckForUpdate());
+            });
+
+            UpdateCheckGitHubButton = new DelegateCommand(async () =>
+            {
+                await Task.Run(() => CheckForUpdateFromGitHub());
             });
         }
 
         void CheckForUpdate()
         {
-            //using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/kuttsun/Squirrel.Windows.Test/releases/latest"))
+            string str = "ローカルチェック" + Environment.NewLine;
+
             using (var mgr = new UpdateManager(@"C:\Users\13005\git\github\Squirrel.Windows.Test\Releases"))
             {
                 try
                 {
-                    var updateInfo = mgr.CheckForUpdate().Result;
+                    var updateinfo = mgr.CheckForUpdate().Result;
 
-                    Console.WriteLine($"Current Version: {updateInfo.CurrentlyInstalledVersion}");
-                    Console.WriteLine($"Latest Version: {updateInfo.FutureReleaseEntry}");
-
-                    foreach (var entry in updateInfo.ReleasesToApply)
-                    {
-                        Console.WriteLine($"- Filename : {entry.Filename}");
-                    }
+                    SetUpdateInfo(ref str, updateinfo);
                 }
                 catch
                 {
-                    Console.WriteLine("No releases.");
+                    str += "Error occurred." + Environment.NewLine;
                 }
+            }
+
+            Result = str;
+        }
+
+        void CheckForUpdateFromGitHub()
+        {
+            string str = "GitHub チェック" + Environment.NewLine;
+
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/kuttsun/Squirrel.Windows.Test/releases/latest"))
+            {
+                try
+                {
+                    var updateinfo = mgr.Result.CheckForUpdate().Result;
+
+                    SetUpdateInfo(ref str, updateinfo);
+                }
+                catch
+                {
+                    str += "Error occurred." + Environment.NewLine;
+                }
+            }
+
+            Result = str;
+        }
+
+        void SetUpdateInfo(ref string str, UpdateInfo updateinfo)
+        {
+            str += $"Current Version: {updateinfo.CurrentlyInstalledVersion?.Version}" + Environment.NewLine;
+            str += $"Latest Version: {updateinfo.FutureReleaseEntry?.Version}" + Environment.NewLine;
+
+            foreach (var entry in updateinfo.ReleasesToApply)
+            {
+                str += $"- Filename : {entry.Filename}" + Environment.NewLine;
             }
         }
     }
